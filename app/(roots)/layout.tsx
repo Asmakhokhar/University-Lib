d7c7c8c2e2394ab96197aaf4157aa5a6286837ae
Asma Khokhar <asma.khokharr@gmail.com>
2026-02-05 19:07:56 +0500

    added setup of modules

---
+++ app/(roots)/layout.tsx
@@ -0,0 +1,17 @@
+import React, { ReactNode } from "react";
+
+
+const Layout = ({ children }: { children:ReactNode }) => {
+  return (
+    <main className="root-container">
+        <div className="mx-auto max-w-7xl text-black">
+            Header        
+            <div className="mt-20 pb-20">
+                {children}
+            </div>
+        </div>
+    </main>
+  )
+};
+
+export default Layout;
